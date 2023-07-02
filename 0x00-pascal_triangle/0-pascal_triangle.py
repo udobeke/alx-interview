4a1ebaacec648ef5b13237e495208f60555dbc66
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-07-02 14:17:56 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-"""Pascal Triangle Interview Challenge"""
+"""
+Alx interview
+"""
 
 
 def pascal_triangle(n):
+    """
+    Returns a list of list of integers representing the pascal triangle of n
+    """
+    result = []
     if n <= 0:
-        return []
+        return result
 
-    triangle = [[1]]
-
-    for i in range(1, n):
-        row = [1]
-        prev_row = triangle[i - 1]
-
-        for j in range(1, i):
-            row.append(prev_row[j - 1] + prev_row[j])
-
-        row.append(1)
-        triangle.append(row)
-
-    return triangle
+    for row in range(n):
+        new_list = []
+        for col in range(row + 1):
+            if col == 0 or col == row:
+                new_list.append(1)
+            else:
+                value = result[-1][col - 1] + result[-1][col]
+                new_list.append(value)
+        result.append(new_list)
+    return result
